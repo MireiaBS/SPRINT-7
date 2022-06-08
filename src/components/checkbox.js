03014ef7123fcf539bd5ec3cba4ce92327bba5ed
MireiaBS <barrufetsoler.mireia@gmail.com>
2022-06-08 13:16:08 +0200

    main

---
+++ src/components/checkbox.js
@@ -0,0 +1,14 @@
+    /* import arrDatos from "../datos";
+    import { useEffect,useState } from "react";
+    import Escena from "./Escena/escena";
+
+    export default function Checkbox() {
+
+        const [price, setPrice] = useState('0')
+
+
+        return (
+            <div>
+                
+            </div> )
+        } */
---
+++ src/datos.js
@@ -0,0 +1,13 @@
+const arrDatos = [
+    {txt: `Una página web (500)`,
+    priceItem: 500
+    },
+    {txt: `Una consultoría SEO (300€)`,
+    priceItem: 300
+    },
+    {txt: `Una campaña de Google Ads (200€)`,
+    priceItem: 200
+    },
+]
+
+export default arrDatos;
