d7585804943cb992c10ee22b1a5e80098440c173
MireiaBS <barrufetsoler.mireia@gmail.com>
2022-06-08 13:13:24 +0200

    cmbios

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import arrDatos from "./datos";
+import Escena from "./components/Escena/escena";
 
 function App() {
+  const [price, setPrice] = useState("0");
+  const [checked, setChecked] = useState({
+    web: false,
+    seo: false,
+    ads: false,
+    pages: 1,
+    languges: 1
+  });
+  console.log(checked)
+ 
+
+  const handleCheck = (event) => {
+    let actualList = [...checked];
+    let target = event.target.checked
+    if (target) {
+      actualList = [...checked, target]
+    } else {
+      
+    }
+    setChecked(actualList);
+    console.log(actualList)
+  }
+
+ /*  let checkedItems = checked.length
+    ? checked.reduce((total, item) => {
+        return total + item;
+      })
+    : 0; */
+  
+
+  const datosPrecios = arrDatos.map((dato, i) => {
+    return (
+      <p key={i}>
+        <input          
+          type="checkbox"
+          name="checkbox"
+          onChange={handleCheck}
+        />{" "}
+        <Escena txt={dato.txt} />
+      </p>
+    );
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3> ¿Que quieres hacer?</h3>
+      {datosPrecios}
+      <p> Precio: {price}€ </p>
     </div>
   );
 }
